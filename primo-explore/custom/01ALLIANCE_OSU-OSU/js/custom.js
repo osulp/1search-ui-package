20390b40a5d3f73355baea895a19c382ad93a441
Hui Zhang <hui.zhang@oregonstate.edu>
2022-01-05 10:55:50 -0500

    Fine tune style, enable toggle institution

--- primo-explore/custom/01ALLIANCE_OSU-OSU/js/custom.js
+++ primo-explore/custom/01ALLIANCE_OSU-OSU/js/custom.js
@@ -3,12 +3,15 @@
 
 /************************************* BEGIN Bootstrap Script ************************************/
 /* We are a CENTRAL_PACKAGE, so use the below line to bootstrap the module */
-// var app = angular.module('viewCustom', ['angularLoad','toggleInstitutions','reportProblem']);
+/* var app = angular.module('viewCustom', ['angularLoad','toggleInstitutions','reportProblem']); */
+
+/*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
+/*var app = angular.module('centralCustom', ['angularLoad']);*/
 /************************************* END Bootstrap Script ************************************/
 
-var app = angular.module('viewCustom', ['oadoi', 'oadoiResults','hathiTrustAvailability', 'angularLoad', 'reportProblem']);
+var app = angular.module('viewCustom', ['angularLoad', 'oadoi', 'oadoiResults','hathiTrustAvailability', 'reportProblem']);
 
-// Add Google Scholar and Worldcat search in facet pane
+/* Add Google Scholar and Worldcat search in facet pane */
 app.component('prmFacetExactAfter', {
     bindings: { parentCtrl: '<' },
     controller: function controller($scope) {
@@ -27,29 +30,28 @@ app.component('prmFacetExactAfter', {
     template: '<div class="{{$ctrl.class}}"><div aria-label="Search in Worldcat" class="section-title md-button md-primoExplore-theme md-ink-ripple layout-fill" style="" ><div class="layout-align-start-center layout-row"><h3 class="section-title-header"><span title="External Search" translate="External Search"></span></h3></div><div class="md-ripple-container"></div></div><div aria-hidden="false" class="section-content animate-max-height-variable" style=""><div class="md-chips md-chips-wrap"><div aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4"><div class="md-chip-content layout-row" role="button" tabindex="0"><strong dir="auto" title="Search Worldcat" ><a href="https://www.worldcat.org/search?qt=worldcat_org_all&q={{$ctrl.query}}" target="_blank"><img src="custom/01ALLIANCE_OSU-OSU/img/worldcat.png" width="22" height="22" alt="worldcat-logo" style="vertical-align:middle;"> Search Worldcat</a></strong></div></div><div aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4"><div class="md-chip-content layout-row" role="button" tabindex="0"><strong dir="auto" title="Search Google Scholar" ><a href="https://scholar.google.com/scholar?q={{$ctrl.query}}" target="_blank"> <img src="custom/01ALLIANCE_OSU-OSU/img/gscholar.png" width="22" height="22" alt="google-scholar-logo" style="vertical-align:middle;"> Google Scholar</a></strong></div></div></div></div>'
 });
 
-/* Hide/Show Summit Institutions - go live July 2018
+/* Hide/Show Summit Institutions - go live July 2018 */
 app.component('prmAlmaMoreInstAfter', { template: '<toggle-institutions />' });
 app.constant('showHideMoreInstOptions', {
 	default_state: "hidden",
 	show_label: "Show Summit libraries",
 	hide_label: "Hide Summit libraries"
 });
-*/
 
-// Add link to ILL in My Account
+/* Add link to ILL in My Account */
 app.component('prmLoansOverviewAfter', {
     bindings: { parentCtrl: '<' },
     controller: function controller($scope, $element) {},
     template: '<div class=tiles-grid-tile><div class="layout-column tile-content"layout=column><div class="layout-column tile-header"layout=column><h2 class="header-link light-text"role=button tabindex=0><span>Interlibrary Loan</span></h2></div><div class="layout-column layout-align-center-center layout-margin layout-padding message-with-icon" layout=column layout-align="center center"><span><a href="https://access.library.oregonstate.edu/illiad.dll?" target="_blank" aria-label="Link to Interlibrary Loan login" tabindex="0">Log into your ILL account</a> to check pending requests and view articles.</span></div></div></div>'
 });
 
-// Add chat widget to header
+/* Add chat widget to header */
 app.component('prmSearchBookmarkFilterAfter', {
     bindings: {},
     template: '<div class="chat"><a ng-href="http://answers.library.oregonstate.edu/widget_standalone.php?hash=848ad121b384a3768c03838752654abb" target="_blank">Live Chat</a></div>'
 });
 
-// Add Report Problem Banner to Full Display
+/* Add Report Problem Banner to Full Display */
 app.constant('reportProblemOptions', {
     message: "Having trouble accessing a resource?",
     button: "Report a Problem",
@@ -65,7 +67,7 @@ angular.module('reportProblem', []).component('prmActionListAfter', {
     }]
 });
 
-// Force users to login to services page
+/* Force users to login to services page */
 /* delete $location.search().isSerivcesPage because it breaks FindIt@OSU. 1-15-2019 */
 app.component('prmAuthenticationAfter', {
     bindings: { parentCtrl: '<' },
@@ -78,7 +80,7 @@ app.component('prmAuthenticationAfter', {
     }
 });
 
-// Show search scopes by default on basic searches
+/* Show search scopes by default on basic searches */
 app.component('prmSearchBarAfter', {
     bindings: { parentCtrl: '<' },
     controller: 'SearchBarAfterController'
@@ -88,14 +90,14 @@ app.controller('SearchBarAfterController', ['angularLoad', function (angularLoad
     vm.parentCtrl.showTabsAndScopes = true;
 }]);
 
-// Analytics
+/* Google Analytics */
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
         (i[r].q = i[r].q || []).push(arguments);
     }, i[r].l = 1 * new Date();a = s.createElement(o), m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-// Google Tag Manager
+/* Google Tag Manager */
 const gtmId = 'GTM-MWMMS9V'
 function addGTM(doc) {
   const newScript = doc.createElement('script')
@@ -125,7 +127,7 @@ app.component('prmBackToLibrarySearchButtonAfter', {
   bindings: { parentCtrl: '<' }
 }); */
 
-//OADOI find open access articles
+/* OADOI find open access articles */
 app.constant('oadoiOptions', {
   "imagePath": "custom/01ALLIANCE_OSU-OSU/img/oa_50.png",
   "email": "library.1search@oregonstate.edu",
@@ -133,7 +135,7 @@ app.constant('oadoiOptions', {
   "showOnResultsPage": true
 });
 
-//OADOI
+/* OADOI */
 /* Based on OaDOI Link developed by Orbis Cascade Alliance's PCSG group
   https://github.com/alliance-pcsg/primo-explore-oadoi-link
 */
@@ -201,7 +203,7 @@ angular.module('oadoi', []).component('prmFullViewServiceContainerAfter', {
 },
 );
 
-//OADOIResults
+/* OADOIResults */
 /* The components are case sensitive, target "oadoiResults" instead of "oadoiresults" */
 angular
   .module('oadoiResults', [])
